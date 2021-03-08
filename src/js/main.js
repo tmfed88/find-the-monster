2e9b5b749002e62b7c84ca396930601b02fd22a9
tmhfed88 <tmhfed88@gmail.com>
2021-03-08 11:08:05 -0600

    updated config for output, package.json updated build script

---
+++ src/js/main.js
@@ -0,0 +1,3 @@
+let app = document.querySelector('#app');
+
+app.innerHTML = "Hello World";
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 mode: 'development',
-  entry: ["regenerator-runtime/runtime.js",'./src/index.js'],
+  entry: './src/js/main.js',
   devServer: {
     contentBase: './dist',
   },
