395e593cc0a4a480260f4c6c32c0527aca975207
tmhfed88 <tmhfed88@gmail.com>
2021-03-08 11:15:43 -0600

    add monsters array, shuffle array

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,37 @@
 let app = document.querySelector('#app');
 
-app.innerHTML = "Hello World";
+let monsters = [
+    'monster1',
+    'monster2',
+    'monster3',
+    'monster4',
+    'monster5',
+    'monster6',
+    'monster7',
+    'monster8',
+    'monster9',
+    'monster10',
+    'monster11',
+    'sock'
+];
+
+let shuffle = array => {
+
+    let currentIndex = array.length;
+    let temporaryValue, randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    console.log(array);
+    return array;
+
+};
+
+// Shuffle the monsters array
+shuffle(monsters);
